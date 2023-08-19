0bb1aa3db6045e87cf6df67ba3b3e57305e4ac61
Huberdoggy <huberdoggy@gmail.com>
2023-08-18 19:11:31 -0500

    wrote out full test for headless nav from login to sending a message to existing connection

--- app/send_invites.py
+++ app/send_invites.py
@@ -3,6 +3,7 @@ from time import sleep
 
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -21,28 +22,67 @@ def initiate_driver(url=None, interval=None):
     # log_path arg prevents 'gecko.log' in project dir. I don't need it
     navigate_social_media(driver, url, slp_timeout)
     driver.quit()
-    driver.stop_client()
     return True
 
 
 def navigate_social_media(driver, url, interval=None):
-    driver.get(f"https://www.{url}")
     driver.maximize_window()
-    driver.find_element(By.XPATH, "//input[@id='session_key']").send_keys(
-        os.getenv("username")
-    )
-    driver.find_element(By.XPATH, "//input[@id='session_password']").send_keys(
-        os.getenv("password")
-    )
-    sleep(3)
-    driver.find_element(By.XPATH, "//button[@type='submit']").send_keys(Keys.RETURN)
-    driver.switch_to.active_element
-    # my_network = driver.find_element(By.PARTIAL_LINK_TEXT, "My Network")
-
-    # clickable = WebDriverWait(driver, interval).until(
-    #     EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, my_network))
-    # )
-    # clickable.click()
-    if interval is not None:
-        sleep(interval)
+    driver.delete_all_cookies()
+    driver.set_page_load_timeout((interval + 20) if interval is not None else 25)
+    driver.implicitly_wait((interval + 5) if interval is not None else 10)
+    driver.get(f"https://www.{url}")
+    driver.refresh()  # In the event it hangs
+    try:
+        driver.find_element(By.XPATH, "//input[@id='session_key']").send_keys(
+            os.getenv("username")
+        )
+        driver.find_element(By.XPATH, "//input[@id='session_password']").send_keys(
+            os.getenv("password")
+        )
+        driver.find_element(By.XPATH, "//button[@type='submit']").send_keys(Keys.RETURN)
+        driver.switch_to.active_element
+        clickable = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[@title='My Network']"))
+        )
+        clickable.click()
+        clickable = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//a[starts-with(@id, 'ember')]//div//div[text()='Connections']",
+                )
+            )
+        )
+        clickable.click()
+        clickable = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//input[@type='search' and @placeholder='Search by name']",
+                )
+            )
+        )
+        clickable.send_keys("<Name from contacts>")
+        clickable = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//button[starts-with(@aria-label, 'Send a message')]",
+                )
+            )
+        )
+        clickable.click()
+        clickable = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (
+                    By.XPATH,
+                    "//*[@data-placeholder='Write a message...']",
+                )
+            )
+        )
+        clickable.send_keys("<Dummy message>")
+        if interval is not None:
+            sleep(interval)
+    except NoSuchElementException as e:
+        print(e.msg)
     return driver
