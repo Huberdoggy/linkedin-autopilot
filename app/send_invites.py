5e3600d943e9cbf8451aa336e2cc306644ec36f9
Huberdoggy <huberdoggy@gmail.com>
2023-08-21 12:18:31 -0500

    switched getting test search name val from dictionary. Added finally block and check_captcha func in send_invites

--- app/send_invites.py
+++ app/send_invites.py
@@ -11,8 +11,8 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from support import xpath_dict
+from selenium.webdriver.support.ui import WebDriverWait
+from support import people_dict, xpath_dict
 from webdriver_manager.firefox import GeckoDriverManager
 
 load_dotenv()
@@ -29,14 +29,26 @@ def initiate_driver(url=None, interval=None):
     return True
 
 
+def check_for_captcha(driver):
+    xpath = '//h1[text()="Let\'s do a quick security check"]'
+    try:
+        driver.find_element(By.XPATH, xpath)
+        return driver
+    except Exception:  # No captcha. Delay not needed
+        return None
+
+
 def navigate_social_media(driver, url, interval=None):
     creds = dict(islice(xpath_dict.items(), None, 3, None))
     cmds = dict(islice(xpath_dict.items(), 3, len(xpath_dict), None))
     # returns iterator in their default ordering - uses start, stop, step
     driver.maximize_window()
     driver.delete_all_cookies()
-    driver.set_page_load_timeout((interval + 20) if interval is not None else 25)
-    driver.implicitly_wait((interval + 5) if interval is not None else 10)
+    driver.set_page_load_timeout(
+        (interval + 20) if interval is not None else 25
+    )  # defaults to 5, passed from main_menu
+    web_wait_timeout = 10
+    # driver.implicitly_wait((interval + 5) if interval is not None else 10)
     driver.get(f"https://www.{url}")
     driver.refresh()  # In the event it hangs
 
@@ -52,41 +64,36 @@ def navigate_social_media(driver, url, interval=None):
                 )
             else:
                 driver.find_element(By.XPATH, f"{creds[key]}").send_keys(Keys.RETURN)
-        two_factor = driver.find_element(
-            By.XPATH,
-            '//h1[text()="Let\'s do a quick security check"]',
-        )
-        if two_factor:
-            print("Extending delay to account for security check...")
-            sleep(25)
-        driver.switch_to.active_element
 
+        if check_for_captcha(driver) is not None:
+            print("Extending delay to account for security check...")
+            sleep((interval + 20) if interval is not None else 25)
+
+        driver.switch_to.active_element
         for key in cmds:
             if key == "name_search_box":
-                clickable = WebDriverWait(driver, 10).until(
+                clickable = WebDriverWait(driver, web_wait_timeout).until(
                     EC.element_to_be_clickable((By.XPATH, f"{cmds[key]}"))
                 )
-                clickable.send_keys("Stephen Huber")
+                clickable.send_keys(people_dict["brother"].get("name"))
                 driver.switch_to.default_content
                 sleep(5)
             elif key == "write_msg_box":
-                clickable = WebDriverWait(driver, 10).until(
+                clickable = WebDriverWait(driver, web_wait_timeout).until(
                     EC.element_to_be_clickable((By.XPATH, f"{cmds[key]}"))
                 )
-                clickable.send_keys("Hey bro. This is an automated message from Python.")
-                clickable.send_keys(Keys.RETURN)
+                clickable.send_keys(people_dict["brother"].get("message"))
             else:
-                clickable = WebDriverWait(driver, 10).until(
+                clickable = WebDriverWait(driver, web_wait_timeout).until(
                     EC.element_to_be_clickable((By.XPATH, f"{cmds[key]}"))
                 )
                 clickable.click()
         sleep(interval) if interval is not None else 5
-    except (
-        selenium.common.exceptions.NoSuchElementException
-        or selenium.common.exceptions.ElementNotInteractableError
-    ) as e:
-        print(e.msg)
-    return driver
+    except selenium.common.exceptions.NoSuchElementException as e:
+        print(f"Error caught:\n\n{e.msg}")
+        sleep(5)
+    finally:
+        return driver
 
 
 # if __name__ == "__main__":
